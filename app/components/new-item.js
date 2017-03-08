1f93526fb961507b4934135ab15cd127a62993ff
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-08 16:29:42 +0800

    created new-item component, successfully loops on seller UI

--- app/components/new-item.js
+++ app/components/new-item.js
@@ -8,9 +8,15 @@ export default Ember.Component.extend({
         price: parseInt(this.get('price')),
         description: this.get('description'),
         quantity: parseInt(this.get('quantity')),
+        image: this.get('image'),
         soldout: false
       };
       this.sendAction('addNewItem',params);
+      this.set('product',"");
+      this.set('price',"");
+      this.set('description',"");
+      this.set('quantity',"");
+      this.set('image',"");
     }
   }
 });
